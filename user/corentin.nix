8f4fd0b1b29d9f02810bc1de5179720ead1b2998
little-dude <corentinhenry@gmail.com>
2020-01-19 16:31:43 +0100

    overlays: add mozilla overlay & install firefox nightly

--- user/corentin.nix
+++ user/corentin.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
-{
+let
+  mozilla-overlays = fetchTarball {
+      url = https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz;
+  };
+
+in {
   imports = [
     ../programs/zsh
     ../programs/neovim
@@ -8,9 +13,21 @@
     ../programs/emacs
   ];
 
+  # Make the mozilla overlays available to home-manager, because they
+  # contain firefox nightly
+  nixpkgs.overlays = [ (import "${mozilla-overlays}") ];
+
+  # Also make the overlay permanent so that we can use the rust
+  # overlays in our projects
+  xdg.configFile."rust-overlay.nix" = {
+    source = "${mozilla-overlays}/rust-overlay.nix";
+    target = "nixpkgs/overlays/rust-overlay.nix";
+  };
+
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
+    latest.firefox-nightly-bin
     # fonts
     hack-font
     iosevka
