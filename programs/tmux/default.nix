493c15b3b89be85f764d8c86f41c5f755fc68626
little-dude <corentinhenry@gmail.com>
2020-01-19 15:48:23 +0100

    tmux: use powerline for the status bar

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -4,10 +4,16 @@
 #   xdg.configFile.".tmux.conf".source = ./tmux.conf;
 # }
 
+with pkgs.python38Packages;
 {
+  home.packages = [ powerline ];
   programs.tmux = {
     enable = true;
-    extraConfig = builtins.readFile ./tmux.conf;
+    extraConfig = ''
+      run-shell "powerline-daemon -q"
+      source ${powerline}/share/tmux/powerline.conf
+      ''
+      + builtins.readFile ./tmux.conf;
   };
 }
 
