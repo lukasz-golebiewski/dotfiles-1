7052242fe559fe3ab95a228a3072ae6b6b9fe2a4
little-dude <corentinhenry@gmail.com>
2020-02-24 13:30:03 +0100

    git config

--- user/corentin.nix
+++ user/corentin.nix
@@ -27,6 +27,7 @@ in {
 
   home.packages = with pkgs; [
     latest.firefox-nightly-bin
+    gitAndTools.diff-so-fancy
     # fonts
     hack-font
     iosevka
@@ -50,5 +51,36 @@ in {
     enable = true;
     userEmail = "corentinhenry@gmail.com";
     userName = "little-dude";
-  };
-}
+    extraConfig = ''
+[push]
+	default = matching
+[core]
+	editor = nvim
+    # excludesfile = /home/corentih/.config/git/gitignore
+	ignorecase = false
+	pager = diff-so-fancy | less --tabs=4 -RFX
+[github]
+	user = little-dude
+[forge]
+	# By default, forge uses the remote named "origin" as the
+	# *upstream*, but we follow a different convention where
+	# "upstream" is the upstream remote and "origin" is our own
+	# fork.
+	remote = upstream
+[color]
+	ui = true
+[color "diff-highlight"]
+	oldNormal = red bold
+	oldHighlight = red bold 52
+	newNormal = green bold
+	newHighlight = green bold 22
+[color "diff"]
+	meta = 11
+	frag = magenta bold
+	commit = yellow bold
+	old = red bold
+	new = green bold
+	whitespace = red reverse
+      '';
+    };
+  }
